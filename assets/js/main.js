a57bf2100a615edebef4e7fb3d70ed980930a796
Marcoseven <marco.caliciotti3@gmail.com>
2021-10-18 13:29:13 +0200

    Creato file javascript
    Inserita la descrizione dell'esercizio con i consigli, tutto in formato commento

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+/* 
+Descrizione:
+Nelle slide trovate una serie di problemi da risolvere. Ciascuno snack é un problema a se, e richiederá alcuni degli strumenti visti fin ora.
+Completate ciascun esercizio, utilizzando quello che avete imparato fin ora.
+
+Consigli:
+- analizzate prima di tutto il problema,
+- scomponetelo in steps
+- definite che strumenti vi occorrono
+- iniziate a risolvere il problema un passo alla volta.
+- Aprite la documentazione di ciascuno degli strumenti che avete rilevato come necessari alla risoluzione del problema
+
+Ripetete quando sopra per ciascuno snack.
+*/
